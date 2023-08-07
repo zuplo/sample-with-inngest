053bc2f38cdef03ad3430809b35bc4227598b1f7
ntotten <nathan@ntotten.com>
2023-08-07 13:37:41 +0000

    updated env

--- modules/trigger.ts
+++ modules/trigger.ts
@@ -2,10 +2,12 @@ import { ZuploContext, ZuploRequest, environment } from "@zuplo/runtime";
 import { Inngest } from "inngest";
 
 export default async function (request: ZuploRequest, context: ZuploContext) {
+  context.log.debug("hello")
   const inngest = new Inngest({
     name: "My Project",
-    env: new URL(request.url).host.split(".")[0],
-    eventKey: environment.INNGEST_EVENT_KEY,
+    env: environment.INNGEST_ENV,
+    eventKey: "jt4Fi1GZISjIKN9OvRiyGcHlRshuH_Fyu1j0k8Bk5TAzdWjTGZgsrNaSgaXAoOLpmurkPm4ApzsuNb4RFwTjlQ",
+    logger: context.log,
   });
 
   // Send your event payload to Inngest
